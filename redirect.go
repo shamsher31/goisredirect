f8c8de76985f98c462bfba0aff2eb9d2f6b94bb2
Shamsher <ansarisam47@gmail.com>
2015-08-18 11:07:02 +0530

    Use maps for lookup

--- redirect.go
+++ redirect.go
@@ -1,20 +1,18 @@
 // Package redirect Check if a number is a redirect HTTP status code
 package redirect // import "github.com/shamsher31/goisredirect"
 
-func get(n int) bool {
-	code := []int{300, 301, 302, 303, 305, 307, 308}
-
-	for _, val := range code {
-		if n == val {
-			return true
-		}
+func Is(s int) bool {
+	code := map[int]int{
+		300: 300,
+		301: 301,
+		302: 302,
+		303: 303,
+		305: 305,
+		307: 307,
+		308: 308,
 	}
-	return false
-}
 
-func Is(n int) bool {
-
-	if get(n) {
+	if _, ok := code[s]; ok {
 		return true
 	}
 	return false
